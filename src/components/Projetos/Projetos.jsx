def6bdf93b46b34e069255af5fc65308d84691ab
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2025-07-29 16:42:14 -0400

    tete

--- src/components/Projetos/Projetos.jsx
+++ src/components/Projetos/Projetos.jsx
@@ -10,31 +10,31 @@ function Projeto(){
 
         <div className="containerGeralCards">
             <Card 
-                imgCard = "../../../public/img/projetos/Agente360.png"
+                imgCard = "../../../../../public/img/projetos/Agente360.png"
                 nomeProjeto ="Agente 360"
                 desc1Projeto = "Projeto criado em parceria com o Senac, FESPMS e o Governo de Mato Grosso do Sul"
                 desc2Projeto = "O Agente 360 foi uma parceria entre a FESPMS o Senac e o Governo de Mato Grosso do Sul que tem o objetivo de criar um jogo em realidade virtual em que o jogador é um agente de saúde e precisa verificar possiveis focos da dengue no mapa, o jogo inclui um site onde será realizado o cadastro de Estados, Cidades, Escolas, Series e Alunos, além de cadastrar o site conta com um sistema  de ranking com os pontos dos alunos que jogaram e os pontos das escolas, que é a somatória de pontos dos alunos pertencente a determinada escola"
             />
             <Card 
-                imgCard = "../../../public/img/projetos/PassControl.png"
+                imgCard = "../../../../../public/img/projetos/PassControl.png"
                 nomeProjeto ="PassControl"
                 desc1Projeto = "Projeto realizado para a empresa Nota Control"
                 desc2Projeto = "Realizado na Fábrica de Software para a empresa NotaControl, o Projeto PassControl consistem em um sistema de controle de senhas de atendimento focado para atendimentos específicos. O projeto contém gerenciamento  de funcionários, controle de atendimentos, relatórios de desempenho e outras funcionalidades"
             />
             <Card 
-                imgCard = "../../../public/img/projetos/FeiraBosqueDaPaz.png"
+                imgCard = "../../../../../public/img/projetos/FeiraBosqueDaPaz.png"
                 nomeProjeto = "Feira Bosque da Paz"
                 desc1Projeto = "Projeto realizado para a Feria Bosque da Paz"
                 desc2Projeto = "Desenvolvimento de uma plataforma online para gerenciar a Feira Bosque da Paz, com recursos para cadastro de expositores, artistas e atrações culturais. O sistema inclui um ambiente administrativo, facilita a comunicação com o público e parceiros, e promove a visibilidade dos produtos regionais, incentivando a inclusão digital e a economia criativa."
             /> 
             <Card 
-                imgCard = "../../../public/img/projetos/ProjetoSitePortifolio.png"
+                imgCard = "../../../../../public/img/projetos/ProjetoSitePortifolio.png"
                 nomeProjeto = "Portifólio"
                 desc1Projeto = "Portifólio criado em React"
                 desc2Projeto = "Portifólio criado com React para o aprimoramento de habilidades e divulgações de trabalhos e projetos pessoais"
             />
             <Card
-                imgCard="../../../public/img/projetos/Pojeto Netflix.png"
+                imgCard="../../../../../public/img/projetos/Pojeto Netflix.png"
                 nomeProjeto="Projeto Netflix"
                 desc1Projeto="Projeto pertencente ao curso Desenvolvimento Web - Front End"
                 desc2Projeto="                                    
@@ -48,7 +48,7 @@ function Projeto(){
             />
 
             <Card
-                imgCard="../../../public/img/projetos/Projeto GodOfWar.png"
+                imgCard="../../../../../public/img/projetos/Projeto GodOfWar.png"
                 nomeProjeto="Projeto GodOfWar"
                 desc1Projeto="Projeto pessoal"
                 desc2Projeto="
