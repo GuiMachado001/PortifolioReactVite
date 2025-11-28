ce7c27b410ea592e5f85c46e840f894975371a33
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2025-11-28 16:15:30 -0300

    Projetos

--- src/components/Projetos/Projetos.jsx
+++ src/components/Projetos/Projetos.jsx
@@ -1,54 +1,74 @@
 import './Projeto.css';
 import Card from './Card/Card';
 
-function Projeto(){
-    return(
-        <div id="projeto">
-            <div className="containerTitleProjeto">
-                <h1>Meus Projetos</h1>
-            </div>
+function Projeto() {
 
-            <div className="containerGeralCards">
-                <Card 
-                    imgCard="/img/projetos/Agente360.png"
-                    nomeProjeto="Agente 360"
-                    desc1Projeto="Projeto criado em parceria com o Senac, FESPMS e o Governo de Mato Grosso do Sul"
-                    desc2Projeto="O Agente 360 foi uma parceria entre a FESPMS o Senac e o Governo de Mato Grosso do Sul que tem o objetivo de criar um jogo em realidade virtual em que o jogador é um agente de saúde e precisa verificar possiveis focos da dengue no mapa, o jogo inclui um site onde será realizado o cadastro de Estados, Cidades, Escolas, Series e Alunos, além de cadastrar o site conta com um sistema de ranking com os pontos dos alunos que jogaram e os pontos das escolas, que é a somatória de pontos dos alunos pertencente a determinada escola"
-                />
-                <Card 
-                    imgCard="/img/projetos/PassControl.png"
-                    nomeProjeto="PassControl"
-                    desc1Projeto="Projeto realizado para a empresa Nota Control"
-                    desc2Projeto="Realizado na Fábrica de Software para a empresa NotaControl, o Projeto PassControl consistem em um sistema de controle de senhas de atendimento focado para atendimentos específicos. O projeto contém gerenciamento  de funcionários, controle de atendimentos, relatórios de desempenho e outras funcionalidades"
-                />
-                <Card 
-                    imgCard="/img/projetos/FeiraBosqueDaPaz.png"
-                    nomeProjeto="Feira Bosque da Paz"
-                    desc1Projeto="Projeto realizado para a Feria Bosque da Paz"
-                    desc2Projeto="Desenvolvimento de uma plataforma online para gerenciar a Feira Bosque da Paz, com recursos para cadastro de expositores, artistas e atrações culturais. O sistema inclui um ambiente administrativo, facilita a comunicação com o público e parceiros, e promove a visibilidade dos produtos regionais, incentivando a inclusão digital e a economia criativa."
-                /> 
-                <Card
-                    imgCard="/img/projetos/PortifolioVideomaker.png"
-                    nomeProjeto="Portifólio VideoMaker"
-                    desc1Projeto="Portfólio desenvolvido para um profissional de criação de vídeos"
-                    desc2Projeto="Site moderno e responsivo, criado para apresentar trabalhos audiovisuais e fortalecer a presença digital de um VideoMaker, destacando projetos com organização e impacto visual."
-                    gitHubProjeto="https://github.com/Guilherme-Machado01/Projeto-Netflix"
-                />
-                <Card 
-                    imgCard="/img/projetos/ProjetoSitePortifolio.png"
-                    nomeProjeto="Portifólio"
-                    desc1Projeto="Portifólio criado em React"
-                    desc2Projeto="Portifólio criado com React para o aprimoramento de habilidades e divulgações de trabalhos e projetos pessoais"
-                />
-                <Card
-                    imgCard="/img/projetos/ProjetoGodOfWar.png"
-                    nomeProjeto="Projeto GodOfWar"
-                    desc1Projeto="Projeto pessoal"
-                    desc2Projeto="O site God Of War teve como abjetivo ser um desafio pessoal. Esse projeto teve, com certeza, uma importância significativa na evolução das hablidades em HTML, CSS e JS."
-                    gitHubProjeto="https://github.com/Guilherme-Machado01/ProjetoGodOfWar"
-                />
+    const listaProjetos = [
+        {
+            nome: "Agente 360",
+            img: "/img/projetos/Agente360.png",
+            descCurta: "Parceria Senac, FESPMS e Governo MS",
+            descLonga: "O Agente 360 foi uma parceria entre a FESPMS, o Senac e o Governo de Mato Grosso do Sul. O objetivo foi criar um jogo em realidade virtual e web onde o jogador atua como agente de saúde combatendo a dengue. Inclui sistema de ranking escolar e gamificação.",
+            link: ""
+        },
+        {
+            nome: "PassControl",
+            img: "/img/projetos/PassControl.png",
+            descCurta: "Sistema para Nota Control",
+            descLonga: "Realizado na Fábrica de Software para a empresa NotaControl, o PassControl é um sistema de gerenciamento de senhas e filas de atendimento. Inclui módulos administrativos, relatórios de performance e gestão de funcionários.",
+            link: ""
+        },
+        {
+            nome: "Feira Bosque da Paz",
+            img: "/img/projetos/FeiraBosqueDaPaz.png",
+            descCurta: "Plataforma de Gestão de Eventos",
+            descLonga: "Plataforma online completa para gerenciamento da Feira Bosque da Paz. Permite cadastro de expositores, artistas e atrações. Conta com área administrativa robusta e vitrine virtual para produtos regionais e economia criativa.",
+            link: ""
+        },
+        {
+            nome: "Portfólio VideoMaker",
+            img: "/img/projetos/PortifolioVideomaker.png",
+            descCurta: "Showcase Audiovisual",
+            descLonga: "Site moderno e responsivo, desenvolvido com foco em performance e visual impactante para apresentar o portfólio de um profissional de vídeo. Design minimalista que prioriza o conteúdo visual.",
+            link: "https://github.com/Guilherme-Machado01/Projeto-Netflix"
+        },
+        {
+            nome: "Meu Portfólio",
+            img: "/img/projetos/ProjetoSitePortifolio.png",
+            descCurta: "Desenvolvido em React",
+            descLonga: "Este projeto! Criado com React.js para demonstrar minhas habilidades em componentização, hooks, animações CSS e design responsivo. Constantemente atualizado com novas tecnologias.",
+            link: ""
+        },
+        {
+            nome: "God of War Tribute",
+            img: "/img/projetos/ProjetoGodOfWar.png",
+            descCurta: "Landing Page Temática",
+            descLonga: "Projeto desafio pessoal focado em UI Design e CSS avançado. Uma Landing Page temática do jogo God of War, explorando efeitos de parallax, transições complexas e tipografia imersiva.",
+            link: "https://github.com/Guilherme-Machado01/ProjetoGodOfWar"
+        }
+    ];
+
+    return (
+        <section id="projeto">
+            <div className="container-projeto">
+                <div className="title-projeto" data-aos="fade-down">
+                    <h2>Meus <span className="highlight">Projetos</span></h2>
+                </div>
+
+                <div className="grid-projetos">
+                    {listaProjetos.map((item, index) => (
+                        <Card 
+                            key={index}
+                            imgCard={item.img}
+                            nomeProjeto={item.nome}
+                            desc1Projeto={item.descCurta}
+                            desc2Projeto={item.descLonga}
+                            gitHubProjeto={item.link}
+                        />
+                    ))}
+                </div>
             </div>
-        </div>
+        </section>
     );
 }
 
