92a89fbba88efa36a108522457aef08e0f9c888c
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2025-11-28 12:30:09 -0300

    Parallax

--- src/components/Parallax/Parallax.jsx
+++ src/components/Parallax/Parallax.jsx
@@ -1,11 +1,16 @@
+import React from 'react';
 import './Parallax.css';
 
-
-function Parallax(){
-    return(
-        <div className='parallax'>
-        </div>
-    );
+function Parallax() {
+  return (
+    <div className='parallax'>
+      <div className="parallax-overlay"></div>
+      <div className="parallax-content" data-aos="zoom-in">
+        <h2>Transformando ideias em código</h2>
+        <p>Design . Desenvolvimento . Inovação</p>
+      </div>
+    </div>
+  );
 }
 
-export default Parallax
+export default Parallax;
