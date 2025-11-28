a7f129956dd8a1859745b1fc0d16c1b9779c3190
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2025-11-28 16:54:41 -0300

    whave

--- src/components/Whave/Whave.jsx
+++ src/components/Whave/Whave.jsx
@@ -1,9 +1,19 @@
-import './Whave.css'
+import React from 'react';
+import './Whave.css';
 
-function Whave(){
-    return(
-        <div className="neon-separator-container">
-            <div className="neon-line"></div>
+function Whave() {
+    return (
+        <div className="cyber-separator-container">
+            {/* Emissor (Esquerda) */}
+            <div className="cyber-dot sender"></div>
+            
+            {/* A Linha (O "Cano") */}
+            <div className="cyber-line">
+                <div className="energy-beam"></div>
+            </div>
+            
+            {/* Receptor (Direita) */}
+            <div className="cyber-dot receiver"></div>
         </div>
     );
 }
