2bb2ae96fe94eb0e66e6ee4b051b21968d8a5ac5
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2025-11-28 12:52:26 -0300

    Projeto

--- src/components/Projetos/Card/Card.jsx
+++ src/components/Projetos/Card/Card.jsx
@@ -1,37 +1,30 @@
 import './Card.css'
+
 function Card(props) {
   return (
-    <div
-      className="card margin transition1"
-      data-aos="fade-right"
-      data-aos-duration="1000"
-      data-aos-easing="ease-in-out"
-    >
-      <div className="content">
-        <div className="back">
-          <div className="back-content">
-            <div className="containerInfo">
-              <div className="containerImgProjeto">
-                <img src={props.imgCard} alt={props.nomeProjeto} />
-              </div>
-
-              <div className="nameProjeto">
-                <span className='spnNameProject'>{props.nomeProjeto}</span>
-                <span className='spnDescProject'>{props.desc1Projeto}</span>
-              </div>
-            </div>
+    <div className="card-container" data-aos="fade-up" data-aos-duration="1000">
+      <div className="card-inner">
+        
+        <div className="card-front">
+          <img src={props.imgCard} alt={props.nomeProjeto} />
+          <div className="card-front-overlay">
+            <h3>{props.nomeProjeto}</h3>
+            <span>{props.desc1Projeto}</span>
           </div>
         </div>
 
-        <div className="front">
-          <div className="flipCardBack">
-            <div className="containerDescBack">
-              <span>{props.desc2Projeto}</span>
-            </div>
-
-
+        <div className="card-back">
+          <div className="card-back-content">
+            <h3>{props.nomeProjeto}</h3>
+            <p>{props.desc2Projeto}</p>
+            {props.gitHubProjeto && (
+              <a href={props.gitHubProjeto} target="_blank" rel="noopener noreferrer" className="btn-project">
+                Ver Projeto <i className="fa-brands fa-github"></i>
+              </a>
+            )}
           </div>
         </div>
+
       </div>
     </div>
   )
