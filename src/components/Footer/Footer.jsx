9a92e1b9e63c47c1b7405c49175e26ec82df01d3
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2025-11-28 13:07:48 -0300

    Footer / Whave

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,30 @@
+import React from 'react';
 import './Footer.css';
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="footerContainer">
-        <p>© {new Date().getFullYear()} Guilherme Machado. Todos os direitos reservados.</p>
-        {/* <p className="footerCredit"><a className='creditos' href="https://guilhermemachadodev.netlify.app/" target='_blank'>Feito por Guilherme Machado</a></p> */}
+    <footer className="main-footer">
+      <div className="footer-container">
+        
+        <div className="footer-col brand-col">
+            <h2 className="footer-logo">Guilherme <span className="highlight">Machado</span></h2>
+            <p className="footer-desc">Desenvolvedor Front-End focado em criar interfaces modernas e experiências digitais excepcionais.</p>
+        </div>
+
+        <div className="footer-col links-col">
+            <h3>Navegação Rápida</h3>
+            <ul>
+                <li><a href="#home">Início</a></li>
+                <li><a href="#sobreMim">Sobre Mim</a></li>
+                <li><a href="#cursos">Formação</a></li>
+                <li><a href="#projeto">Projetos</a></li>
+            </ul>
+        </div>
+
+      </div>
+
+      <div className="footer-bottom">
+        <p>© {new Date().getFullYear()} Guilherme Machado. Code with <i className="fa-solid fa-heart highlight"></i> and React.</p>
       </div>
     </footer>
   );
