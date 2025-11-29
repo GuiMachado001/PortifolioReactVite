db3891c00ba13a4a6ab52879eb69dfcaa5d8f7ff
Guilherme Machado Neves <gui.m.neves01@gmail.com>
2025-11-29 11:14:56 -0300

    Correções responsividade

--- src/components/Achievements/Achievements.jsx
+++ src/components/Achievements/Achievements.jsx
@@ -4,28 +4,28 @@ import './Achievements.css';
 function Achievements() {
     const listaConquistas = [
         {
-            image: "/img/Achievements/congratulacao.jpeg", // Substitua pelo caminho real da sua foto
+            image: "/img/Achievements/congratulacao.jpeg",
             icon: "fa-solid fa-trophy",
             title: "Moção de Congratulação",
             subtitle: "Câmara Municipal de Campo Grande",
             description: "Reconhecimento oficial pelo impacto social e inovação das soluções desenvolvidas na Fábrica de Software."
         },
         {
-            image: "/img/Achievements/Reportagem.png", // Substitua pelo caminho real da sua foto
+            image: "/img/Achievements/Reportagem.png",
             icon: "fa-solid fa-newspaper",
             title: "Destaque na Mídia Local",
             subtitle: "Jornais e Portais de Notícias",
             description: "Cobertura da imprensa sobre os projetos desenvolvidos durante o estágio e o sucesso da nossa metodologia de trabalho."
         },
         {
-            image: "/img/Achievements/FabricaDeSoftware.jpeg", // Substitua pelo caminho real da sua foto
+            image: "/img/Achievements/FabricaDeSoftware.jpeg",
             icon: "fa-solid fa-people-group",
             title: "Fábrica de Software",
             subtitle: "Formação Concluída (Senac Hub Academy)",
             description: "Conclusão de um programa intensivo focado em metodologias ágeis, trabalho em equipe e desenvolvimento de projetos reais."
         },
         {
-            image: "/img/Achievements/Diploma.png", // Substitua pelo caminho real da sua foto
+            image: "/img/Achievements/Diploma.png",
             icon: "fa-solid fa-graduation-cap",
             title: "Análise e Desenvolvimento",
             subtitle: "Graduação (Estácio)",
